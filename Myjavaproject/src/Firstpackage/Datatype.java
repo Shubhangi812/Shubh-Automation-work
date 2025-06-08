338b69985ab094748e2f66604d0d78ca6304919d
ap2_i <ap2_i@Pravin>
2025-06-08 15:20:31 +0530

    "Average marks print"

---
+++ Myjavaproject/src/Firstpackage/Datatype.java
@@ -0,0 +1,22 @@
+package Firstpackage;
+
+public class Datatype {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a=10;
+		
+		System.out.println(a);
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}

5e14d5e991e409d54ea3d8dad421141a11373d28
ap2_i <ap2_i@Pravin>
2025-06-08 15:13:43 +0530

    "Print Average if the marks project"

---
+++ Myjavaproject/src/Firstpackage/Average_of_marks.java
@@ -0,0 +1,24 @@
+package Firstpackage;
+
+public class Average_of_marks {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	    int math = 65;
+	    int science = 75;
+	    int english = 90;
+	    
+	    int total= math+science+english;
+	    double average= total/3.0;
+	    
+	    System.out.println("Maths: "+math);
+	    System.out.println("Maths: "+science);
+	    System.out.println("Maths: "+english);
+	    System.out.println("total marks of subjects: "+total);
+	    System.out.println("average marks : "+average);
+
+	    
+	}
+
+}
